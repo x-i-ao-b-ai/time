da7635b8b56ea8d77bd7e819fdddfc0befbb7e51
xiaobai <xiaobai@asyou.me>
2015-12-11 09:34:39 +0800

    fixed

--- index.android.js
+++ index.android.js
@@ -8,37 +8,33 @@ var React = require('react-native');
 var UserApi = require('./UserApi');
 
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  ToolbarAndroid,
-  DrawerLayoutAndroid,
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    ToolbarAndroid,
+    DrawerLayoutAndroid,
 } = React;
 
 var timeline = React.createClass({
-  render: function() {
-    var navigationView = (
-      <View style={{flex: 1, backgroundColor: '#25323B'}}>
-        <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>Im in the Drawer!</Text>
-      </View>
-    );
-    return (
-         <DrawerLayoutAndroid
-          drawerWidth={300}
-          drawerPosition={DrawerLayoutAndroid.positions.Left}
-          renderNavigationView={() => navigationView}>
-             <View style={{flex: 1, alignItems: 'center'}}>
-                 <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>Hello</Text>
-                 <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>World!</Text>
-             </View>
-         </DrawerLayoutAndroid>
-    );
-  },
-  onChangeText:function(){
- 
-  }
+    render: function() {
+        var navigationView = (
+            <View style={{flex: 1, backgroundColor: '#25323B'}}>
+                 <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>Im in the Drawer!</Text>
+            </View>
+        );
+        return (
+            <DrawerLayoutAndroid
+              drawerWidth={300}
+              drawerPosition={DrawerLayoutAndroid.positions.Left}
+              renderNavigationView={() => navigationView}>
+                 <View style={{flex: 1, alignItems: 'center'}}>
+                     <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>Hello</Text>
+                 </View>
+            </DrawerLayoutAndroid>
+        );
+    }
 });
 
 var styles = StyleSheet.create({
@@ -46,20 +42,19 @@ var styles = StyleSheet.create({
 });
 
 UserApi.Open("192.168.10.53",9090,
-  (msg) => {
-    console.log(msg);
-  },
-  (x) => {
-    UserApi.Register("222",
-      (msg) => {
+    (msg) => {
         console.log(msg);
-      },
-      (x) => {
-        console.log(x);
-      }
-    );
-  }
+    },
+    (x) => {
+        UserApi.Register("222",
+            (msg) => {
+                console.log(msg);
+            },
+            (x) => {
+                console.log(x);
+            }
+        );
+    }
 );
-
 
 AppRegistry.registerComponent('timeline', () => timeline);
